28924953fcb5c106c24de65115addc25da6d42bf
alex-501 <68447140+alex-501@users.noreply.github.com>
2021-05-19 17:58:47 -0700

    Update reportWebVitals.js

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,30 @@
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
-};
+//http://copilot.devatics.com:7990/users/smaurel/repos/front-interview-react/browse/src/reportWebVitals.js?at=7c81980
+//History
 
+// const reportWebVitals = onPerfEntry => {
+//   if (onPerfEntry && onPerfEntry instanceof Function) {
+//     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+//       getCLS(onPerfEntry);
+//       getFID(onPerfEntry);
+//       getFCP(onPerfEntry);
+//       getLCP(onPerfEntry);
+//       getTTFB(onPerfEntry);
+//     });
+//   }
+// };
+
+export default reportWebVitals;
+
+
+
+
+const reportWebVitals = onPerfEntry =>
+ {  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);    getFID(onPerfEntry);
+      getFCP(onPerfEntry);      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);    });}};
+
+
+      //export reportWebVitals
 export default reportWebVitals;
