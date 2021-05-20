a515a3466f1e52604dca6cf71c0bfcf83f70d3bb
alex-501 <68447140+alex-501@users.noreply.github.com>
2021-05-19 18:44:04 -0700

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,32 @@
+
+
+//import react
+//../assets/images/ ...
+import React from 'react';
+import personalImage from "../assets/images/stephanie-porfolio-pic.jpeg";
+//about section
+function About() {
+  return (
+
+    <section className="col-10" id="about-me">
+    <h1>About Me</h1>
+
+    <div className="row justify-content-center" id="about-me-image">
+      <img src={personalImage}  /></div>
+    <div className="row justify-content-center" id="about-me-text">
+      <div className="col-sm-12 col-lg-2"></div>
+      <div className="col-sm-12 col-lg-8">
+        <p>
+        My name is Alexis Rodriguez, I am a full stack developer, and was a Sawyer and a welder in the Past
+        </p>
+        
+
+      </div>
+      <div className="col-sm-12 col-lg-2"> </div> </div>
+  </section>
+
+
+  );
+}
+
+export default About;
