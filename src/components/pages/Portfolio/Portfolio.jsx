4efd66a743a2ba5162435e84f335ee53b16010d2
alex-501 <68447140+alex-501@users.noreply.github.com>
2021-05-21 17:42:47 -0700

    Update Portfolio.jsx

--- src/components/pages/Portfolio/Portfolio.jsx
+++ src/components/pages/Portfolio/Portfolio.jsx
@@ -1,28 +1,19 @@
-import styles from "./portfolio.module.css";
+//./portfolioItem
 import PortfolioItem from "./PortfolioItem";
 import { portfolioItems } from "./portfolioItems";
+//styles
+import styles from "./portfolio.module.css";
 
-const Portfolio = () => {
-    return (
-        <section className={styles.portfolio}>
-            <div className={styles.heading}>
-                <h2>Portfolio</h2>
-                <h4>Recent Projects</h4>
-            </div>
+
+//define
+const Portfolio = () => {return (
+        <section className={styles.portfolio}> <div className={styles.heading}>       <h2>Portfolio</h2>
+                <h4>Class Projects</h4> </div>
 
             <div className={styles.galleryWrapper}>
-                {portfolioItems.map((item, i) => (
-                    <PortfolioItem key={i} item={item} />
-                ))}
-            </div>
-
-            <div className={styles.buttonBox}>
-                <a href="/" className={styles.button}>
-                    See More &rarr;
-                </a>
-            </div>
-        </section>
-    );
-};
-
+         {portfolioItems.map((item, i) => 
+                (  <PortfolioItem key={i} item={item} /> ))}    </div>
+    <div className={styles.buttonBox}>
+                <a href="/" className={styles.button}> See More &rarr;</a> </div> </section> );};
+//export Portfolio ^
 export default Portfolio;
