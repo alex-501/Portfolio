04c1ca686913302645150fe0571007d48b29b4a1
alex-501 <68447140+alex-501@users.noreply.github.com>
2021-05-21 18:07:50 -0700

    resume needs work

--- src/components/pages/Resume/Resume.jsx
+++ src/components/pages/Resume/Resume.jsx
@@ -1,5 +1,4 @@
 import styles from "./resume.module.css";
-import { backEndSkills, frontEndSkills } from "./skills";
 const Resume = () => {
     return (
         <section className={styles.resume}>
@@ -7,42 +6,9 @@ const Resume = () => {
                 <h2>My Resume</h2>
             </div>
 
-            <p className={styles.resume__link}>
-                Download My{" "} 
-                <a
-                    className={styles.button}
-                    // ADD GOOGLE DRIVE RESUME WHEN UPDATED
-                    href="https://www.linkedin.com/in/justinbeard1989/"
-                >
-                    Resume
-                </a>{" "} Here
-            </p>
-
-            <div className={styles.frontEnd}>
-                <h2 className={styles.frontEndHeader}>Front-End Skills</h2>
-                <ul className={styles.list}>
-                    {frontEndSkills.map((skill, i) => (
-                        <li key={i} className={styles.listItem}>
-                            {skill.title}
-                            <span> {skill.icon}</span>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-
-            <div className={styles.backend}>
-                <h2 className={styles.backendHeader}>Back-End Skills</h2>
-                <ul className={styles.list}>
-                    {backEndSkills.map((skill, i) => (
-                        <li key={i} className={styles.listItem}>
-                            {skill.title}
-                            <span> {skill.icon}</span>
-                        </li>
-                    ))}
-                </ul>
-            </div>
         </section>
     );
 };
 
+//resume export
 export default Resume;
