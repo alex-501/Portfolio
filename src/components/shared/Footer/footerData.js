2a09fdb1c0d063eaf8dbbf49b1fd3d68ffc4cf18
alex-501 <68447140+alex-501@users.noreply.github.com>
2021-05-21 18:17:50 -0700

    footer links

--- src/components/shared/Footer/footerData.js
+++ src/components/shared/Footer/footerData.js
@@ -1,8 +1,8 @@
 
-import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+//import { } from "./";
+import { AiFillGithub } from "react-icons/ai";
 
 export const footerData = [
-    { title: "Github", link: "https://github.com/ATXiceman512", icon: <AiFillGithub /> },
-    { title: "LinkedIn", link: "https://www.linkedin.com/in/justinbeard1989/", icon: <AiFillLinkedin /> },
-
-]
+    { title: "Github",
+     link: "https://github.com/alex-501", 
+     icon: <AiFillGithub /> },]
