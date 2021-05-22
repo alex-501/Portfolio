b81848a19ae2a9268813816616c27873fc9a92b7
alex-501 <68447140+alex-501@users.noreply.github.com>
2021-05-21 18:14:14 -0700

    footer update

--- src/components/shared/ErrorPage/ErrorPage.jsx
+++ src/components/shared/ErrorPage/ErrorPage.jsx
@@ -1,23 +1,12 @@
+
+//react & router
 import React from "react";
 import { Link } from "react-router-dom";
-
+//define err and put back link "/"
 const ErrorPage = () => {
-    return (
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                flexDirection: "column",
-            }}
-        >
-            <h1>Woops!!!!</h1>
-            <h3>Looks like this page doesn't exists</h3>
-            <h4>
-                Let's head back <Link to="/">Home</Link>
-            </h4>
-        </div>
-    );
-};
-
+    return (<div
+            style={{display: "flex",justifyContent: "center",          alignItems: "center",      flexDirection: "column", }}>
+            <h1>Mortal Error</h1>
+            <h3> <Link to="/">Return</Link></h3> </div>);};
+//export err page
 export default ErrorPage;
--- src/components/shared/Footer/Footer.jsx
+++ src/components/shared/Footer/Footer.jsx
@@ -1,21 +1,9 @@
-import styles from "./Footer.module.css";
-import { footerData } from "./footerData";
-
-const Footer = () => {
-    return (
-        <footer className={styles.footer}>
-            {footerData.map((data, i) => (
-                <a
-                    key={i}
-                    href={data.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    {data.icon}
-                </a>
-            ))}
-        </footer>
-    );
-};
-
+//footer plus styles
+import { footerData } from "./footerData";  import styles from "./Footer.module.css";
+const Footer = () => {return (
+<footer className={styles.footer}>     {footerData.map((data, i) => (
+                <a     key={i}      href={data.link}    target="_blank"   rel="noopener noreferrer">
+                    {data.icon} </a> ))}
+</footer>);};
 export default Footer;
+//EXPORT FOOTER
